3041a3e3273dd5263f86df30867f4997afefbbb5
liamdavies1988 <liam.davies1988@outlook.com>
2026-02-27 15:15:15 +0000

    fixed animation buttons
    finally fixed animation buttons by hard coding a bool for right and wrong answer animations within the on click function

--- Assets/Scripts/AnswerButtons.cs
+++ Assets/Scripts/AnswerButtons.cs
@@ -35,25 +35,52 @@ public class AnswerButton : MonoBehaviour
 
     public void OnClick()
     {
-        if (anim == null) anim = GetComponent<Animator>(); // Double check we have the animator
-
-        if (isCorrectAnswer)
+        if (anim == null) anim = GetComponent<Animator>();// Double check we have the animator
         {
-            Debug.Log("Correct!");
-            anim.SetTrigger("Pressed");
-            GameManager.Instance.AddCoin();
-            StartCoroutine(NextQuestionDelay());
-        }
-        else
-        {
-            Debug.Log("Wrong!");
-            // Make sure "WrongAnswer" matches the name in your Parameters tab EXACTLY
-            anim.SetTrigger("WrongAnswer");
+            if (isCorrectAnswer)
+            {
+                Debug.Log("Correct!");
 
-            
+                PlayFeedbackAnimation(true);// Play the correct answer animation
+                GameManager.Instance.AddCoin();// Player wins a coin
+                StartCoroutine(NextQuestionDelay());// Starts next question after delay, giving time for the animation to play
+            }
+            else if (anim != null)
+            {
+                Debug.Log("Wrong!");
+
+                // Change from SetTrigger to SetBool
+                anim.SetBool("IsWrong", true);// This will trigger the wobble animation in the Animator
+
+                // We MUST turn it back to false after a delay, 
+                // otherwise the button will stay in the 'Wrong' state forever.
+                StartCoroutine(ResetWrongBool());// This will reset the bool after the wobble animation is done
+                StartCoroutine(NextQuestionDelay());// Starts next question after delay, giving time for the animation to play
+
+            }
         }
     }
 
+    IEnumerator ResetWrongBool()
+    {
+        yield return new WaitForSeconds(0.5f); // Length of your wobble
+        anim.SetBool("IsWrong", false);
+    }
+
+    // This is the clean function you wanted!
+    private void PlayFeedbackAnimation(bool isRight)
+    {
+        if (isRight)
+        {
+            // Tell animator to play the Right sequence
+            anim.SetTrigger("Pressed");
+        }
+        else
+        {
+            // Tell animator to play the Wrong sequence
+            anim.SetTrigger("WrongAnswer");
+        }
+    }
     IEnumerator NextQuestionDelay()
     {
         // Wait so the child can see the reward/animation
